42885b5f923b7a2c54787fdeeed79bf0483ae319
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-08-27 18:22:05 +0530

    app

--- App.js
+++ App.js
@@ -146,125 +146,153 @@ export default function App() {
     //     height: 300
     //   }}/>
     // </View>
-    <View style={{
-      backgroundColor: 'white',
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <View style={{
-        backgroundColor: 'blue',
-        width: 100,
-        height: 100
-      }}/>
-      <View style={{
-        backgroundColor: 'gold',
-        width: 100,
-        height: 100
-      }}/>
-      <View style={{
-        backgroundColor: 'tomato',
-        width: 100,
-        height: 100
-      }}/>
-    </View>
-    // <SafeAreaView style={styles.container}>
-    //   <ScrollView style={styles.scrollView}>
-    //     <View style={{
-    //       backgroundColor: 'red',
-    //       width: '100%',
-    //       height: landscape ? '100%' : '0%'
-    //     }}>
-    //       <Text>View</Text>
-    //     </View>
-    //     <View style={containerStyle}>
-    //       <Text>1111111111111111</Text>
-    //       <StatusBar style='auto'/>
-    //     </View>
-    //     <View style={styles.container}>
-    //       <Text>22222222222222222222</Text>
-    //     </View>
-    //     <View style={[styles.container, containerStyle]}>
-    //       <Text>33333333333333333333333</Text>
-    //     </View>
-    //     <Text>
-    //       Open up App.js to start working on your app!
-    //     </Text>
-    //     <Text>
-    //       Open up App.js to start working on your app!
-    //       I am going to make this a very long text.
-    //       Now I am going to make this even longer to see what happens!
-    //     </Text>
-    //     <Text numberOfLines={1}>
-    //       Open up App.js to start working on your app!
-    //       I am going to make this a very long text.
-    //       Now I am going to make this even longer to see what happens!
-    //     </Text>
-    //     <Text numberOfLines={2}>
-    //       Open up App.js to start working on your app!
-    //       I am going to make this a very long text.
-    //       Now I am going to make this even longer to see what happens!
-    //     </Text>
-    //     <Text onPress={() => console.log('Text Pressed 1')}>
-    //       Open up App.js to start working on your app!
-    //     </Text>
-    //     <Text onPress={() => {
-    //       console.log('Text Pressed 2')
-    //     }}>
-    //       Open up App.js to start working on your app!
-    //     </Text>
-    //     <Text onPress={handlePressed}>
-    //       Open up App.js to start working on your app!
-    //     </Text>
-    //     <Image source={require('./assets/icon.png')}/>
-    //     <TouchableWithoutFeedback onPress={handleImagePressed1}>
-    //       <Image blurRadius={2}
-    //              fadeDuration={1000}
-    //              source={{
-    //                width: 200,
-    //                height: 100,
-    //                uri: 'https://picsum.photos/200/100'
-    //              }}/>
-    //     </TouchableWithoutFeedback>
-    //     <TouchableOpacity onPress={handleImagePressed2}>
-    //       <Image blurRadius={0}
-    //              fadeDuration={1000}
-    //              source={{
-    //                width: 100,
-    //                height: 100,
-    //                uri: 'https://picsum.photos/100'
-    //              }}/>
-    //     </TouchableOpacity>
-    //     <TouchableHighlight onPress={handleImagePressed3}>
-    //       <Image blurRadius={0}
-    //              fadeDuration={1000}
-    //              source={{
-    //                width: 100,
-    //                height: 100,
-    //                uri: 'https://picsum.photos/100'
-    //              }}/>
-    //     </TouchableHighlight>
-    //     <TouchableNativeFeedback onPress={handleImagePressed4}>
-    //       <Image blurRadius={0}
-    //              fadeDuration={1000}
-    //              source={{
-    //                width: 100,
-    //                height: 100,
-    //                uri: 'https://picsum.photos/100'
-    //              }}/>
-    //     </TouchableNativeFeedback>
-    //     <View style={{
-    //       width: 200,
-    //       height: 70,
-    //       backgroundColor: 'blue'
-    //     }}/>
-    //     <Button onPress={handleButtonTapped}
-    //             title={'Click Me'}
-    //             color='orange'/>
-    //   </ScrollView>
-    //   <StatusBar style='auto'/>
-    // </SafeAreaView>
+    // <View style={{
+    //   backgroundColor: 'white',
+    //   flex: 1,
+    //   flexDirection: 'row',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   alignContent: 'center',
+    //   flexWrap: 'wrap'
+    // }}>
+    //   <View style={{
+    //     backgroundColor: 'blue',
+    //     width: 100,
+    //     // flexBasis: 400,
+    //     // flexShrink: 1,
+    //     // flex: -1,
+    //     // height: 300
+    //     // flex: 1,
+    //     // flexGrow: 1,
+    //     height: 100
+    //   }}/>
+    //   <View style={{
+    //     backgroundColor: 'gold',
+    //     width: 100,
+    //     height: 100,
+    //     // position: 'relative',
+    //     position: 'absolute',
+    //     // top: 20,
+    //     // top: -20,
+    //     // bottom: 20,
+    //     // bottom: -20,
+    //     // right: 20,
+    //     // right: -20,
+    //     // left: 20,
+    //     left: -20
+    //   }}/>
+    //   <View style={{
+    //     backgroundColor: 'tomato',
+    //     width: 100,
+    //     height: 100
+    //   }}/>
+    //   {/*<View style={{*/}
+    //   {/*  backgroundColor: 'grey',*/}
+    //   {/*  width: 100,*/}
+    //   {/*  height: 100*/}
+    //   {/*}}/>*/}
+    //   {/*<View style={{*/}
+    //   {/*  backgroundColor: 'greenyellow',*/}
+    //   {/*  width: 100,*/}
+    //   {/*  height: 100*/}
+    //   {/*}}/>*/}
+    // </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <View style={{
+          backgroundColor: 'red',
+          width: '100%',
+          height: landscape ? '100%' : '0%'
+        }}>
+          <Text>View</Text>
+        </View>
+        <View style={containerStyle}>
+          <Text>1111111111111111</Text>
+          <StatusBar style='auto'/>
+        </View>
+        <View style={styles.container}>
+          <Text>22222222222222222222</Text>
+        </View>
+        <View style={[styles.container, containerStyle]}>
+          <Text>33333333333333333333333</Text>
+        </View>
+        <Text>
+          Open up App.js to start working on your app!
+        </Text>
+        <Text>
+          Open up App.js to start working on your app!
+          I am going to make this a very long text.
+          Now I am going to make this even longer to see what happens!
+        </Text>
+        <Text numberOfLines={1}>
+          Open up App.js to start working on your app!
+          I am going to make this a very long text.
+          Now I am going to make this even longer to see what happens!
+        </Text>
+        <Text numberOfLines={2}>
+          Open up App.js to start working on your app!
+          I am going to make this a very long text.
+          Now I am going to make this even longer to see what happens!
+        </Text>
+        <Text onPress={() => console.log('Text Pressed 1')}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Text onPress={() => {
+          console.log('Text Pressed 2')
+        }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Text onPress={handlePressed}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Image source={require('./assets/icon.png')}/>
+        <TouchableWithoutFeedback onPress={handleImagePressed1}>
+          <Image blurRadius={2}
+                 fadeDuration={1000}
+                 source={{
+                   width: 200,
+                   height: 100,
+                   uri: 'https://picsum.photos/200/100'
+                 }}/>
+        </TouchableWithoutFeedback>
+        <TouchableOpacity onPress={handleImagePressed2}>
+          <Image blurRadius={0}
+                 fadeDuration={1000}
+                 source={{
+                   width: 100,
+                   height: 100,
+                   uri: 'https://picsum.photos/100'
+                 }}/>
+        </TouchableOpacity>
+        <TouchableHighlight onPress={handleImagePressed3}>
+          <Image blurRadius={0}
+                 fadeDuration={1000}
+                 source={{
+                   width: 100,
+                   height: 100,
+                   uri: 'https://picsum.photos/100'
+                 }}/>
+        </TouchableHighlight>
+        <TouchableNativeFeedback onPress={handleImagePressed4}>
+          <Image blurRadius={0}
+                 fadeDuration={1000}
+                 source={{
+                   width: 100,
+                   height: 100,
+                   uri: 'https://picsum.photos/100'
+                 }}/>
+        </TouchableNativeFeedback>
+        <View style={{
+          width: 200,
+          height: 70,
+          backgroundColor: 'blue'
+        }}/>
+        <Button onPress={handleButtonTapped}
+                title={'Click Me'}
+                color='orange'/>
+      </ScrollView>
+      <StatusBar style='auto'/>
+    </SafeAreaView>
   )
 }
 
